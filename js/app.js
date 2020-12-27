694b4384023167da5ecfe488c71a44f0392d29f7
Andrea Fernandes <andreaisabelfreitas@gmail.com>
2020-12-27 01:42:34 +0000

    Adicionei o 2do Scenario
    + Adicionei o 2do Scenario.
    
    + Pus os elementos a encontrar.
    
    + Fiz a lista com os objetos
      a encontrar.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ let camera;
 let renderer;
 let scene;
 let house;
-let planta;
-let plantaScene;
+let planta, plantaScene;
+let cereal, cerealScene;
+let cafetera, cafeteraScene;
+let ovo, ovoScene;
+let bolacha, bolachaScene;
+let papel, papelScene;
+
 let canvas;
-let textoObjectoPerdido1CheckMesh;
+let textoObjectoPerdido1CheckMesh, textoObjectoPerdido2CheckMesh;
 
 function updateCanvasFullScreen() {
     canvas.width = window.innerWidth;
@@ -119,6 +124,278 @@ function init() {
 
     animate();
 }
+
+function init2() {
+    canvas = document.getElementById("webGLCanvas");
+
+    updateCanvasFullScreen()
+
+    /* Renderer */
+    renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
+    renderer.setSize(canvas.width, canvas.height);
+
+    /* Criar Scene */
+    scene = new THREE.Scene();
+
+    /* Criar Camera */
+    camera = new THREE.PerspectiveCamera(85, canvas.width / canvas.height, 1, 1000);
+    camera.position.set(-2.230, 4.030, 8.310);
+
+    scene.add(camera);
+
+    /*
+        ##################################
+        #     Controlos do Three         #
+        ##################################
+        */
+    //let controls = new THREE.OrbitControls(camera);
+    //controls.addEventListener('change', function() { renderer.render(scene, camera); });
+
+
+    /* Adicionar a possibilidade de mapear eventos dom a elementos 3D */
+    const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
+
+
+    /* Criar Luz Ambiente */
+    const ambient = new THREE.AmbientLight(0x404040, 2);
+    scene.add(ambient);
+
+    /* Criar Luz Direcional */
+    const light = new THREE.DirectionalLight(0xffffff, 2);
+    light.position.set(50, 50, 100);
+    scene.add(light);
+
+    /* Carregar a sala */
+    let loader = new THREE.GLTFLoader();
+    loader.load("./GLTFs/sceneLevelTwo/cozinha/scene_Kitchen.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        house = gltf.scene.children[0];
+        gltf.scene.position.set(-5.390, -0.290, -4.640);
+        gltf.scene.rotation.set(0, 0.5, 0);
+    });
+
+    /* Carregar a fonte e alguns textos */
+    const loaderFont = new THREE.FontLoader();
+
+    let myFont = loaderFont.load('./fonts/Roboto_Regular.json', function(font) {
+
+        const textGeometry = new THREE.TextGeometry("Objectos Perdidos:", {
+            font: font,
+            size: 0.6,
+            height: 0.01,
+        });
+
+        const textGeometryObjecto1 = new THREE.TextGeometry("Cereal", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+
+        const textGeometryObjecto2 = new THREE.TextGeometry("Cafetera", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+        const textGeometryObjecto3 = new THREE.TextGeometry("Bolachas", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+        const textGeometryObjecto4 = new THREE.TextGeometry("Ovo Partido", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+        const textGeometryObjecto5 = new THREE.TextGeometry("Papel", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+
+        const textGeometryObjecto1Check = new THREE.TextGeometry("X", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+
+        const textGeometryObjecto2Check = new THREE.TextGeometry("X", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+
+        const textGeometryObjecto3Check = new THREE.TextGeometry("X", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+        const textGeometryObjecto4Check = new THREE.TextGeometry("X", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+        const textGeometryObjecto5Check = new THREE.TextGeometry("X", {
+            font: font,
+            size: 0.4,
+            height: 0.01,
+        });
+
+
+
+        let textMaterial2 = new THREE.MeshBasicMaterial({ color: 0xff3535 });
+
+
+        let textMaterial = new THREE.MeshBasicMaterial({ color: 0x191919 });
+
+        /* Adicionar o titulo de objectos perdidos */
+        let textoObjectosPerdidosMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textoObjectosPerdidosMesh.position.set(5, 10, 0);
+        scene.add(textoObjectosPerdidosMesh);
+
+        /* Adicionar o objeto 1 */
+        let textoObjectoPerdido1Mesh = new THREE.Mesh(textGeometryObjecto1, textMaterial);
+        textoObjectoPerdido1Mesh.position.set(5, 8.5, 0);
+        scene.add(textoObjectoPerdido1Mesh);
+
+        /* Marcar como found */
+        textoObjectoPerdido1CheckMesh = new THREE.Mesh(textGeometryObjecto1Check, textMaterial2);
+        textoObjectoPerdido1CheckMesh.position.set(4, 8.5, 0);
+        scene.add(textoObjectoPerdido1CheckMesh);
+        textoObjectoPerdido1CheckMesh.visible = false
+
+        /* Adicionar o objeto 2 */
+        let textoObjectoPerdido2Mesh = new THREE.Mesh(textGeometryObjecto2, textMaterial);
+        textoObjectoPerdido2Mesh.position.set(5, 7.5, 0);
+        scene.add(textoObjectoPerdido2Mesh);
+
+        /* Marcar como found */
+        textoObjectoPerdido2CheckMesh = new THREE.Mesh(textGeometryObjecto2Check, textMaterial2);
+        textoObjectoPerdido2CheckMesh.position.set(4, 7.5, 0);
+        scene.add(textoObjectoPerdido2CheckMesh);
+        textoObjectoPerdido2CheckMesh.visible = false
+
+        /* Adicionar o objeto 3 */
+        let textoObjectoPerdido3Mesh = new THREE.Mesh(textGeometryObjecto3, textMaterial);
+        textoObjectoPerdido3Mesh.position.set(5, 6.5, 0);
+        scene.add(textoObjectoPerdido3Mesh);
+
+        /* Marcar como found */
+        textoObjectoPerdido3CheckMesh = new THREE.Mesh(textGeometryObjecto3Check, textMaterial2);
+        textoObjectoPerdido3CheckMesh.position.set(4, 6.5, 0);
+        scene.add(textoObjectoPerdido3CheckMesh);
+        textoObjectoPerdido3CheckMesh.visible = false
+
+        /* Adicionar o objeto 4 */
+        let textoObjectoPerdido4Mesh = new THREE.Mesh(textGeometryObjecto4, textMaterial);
+        textoObjectoPerdido4Mesh.position.set(5, 5.5, 0);
+        scene.add(textoObjectoPerdido4Mesh);
+
+        /* Marcar como found */
+        textoObjectoPerdido4CheckMesh = new THREE.Mesh(textGeometryObjecto4Check, textMaterial2);
+        textoObjectoPerdido4CheckMesh.position.set(4, 5.5, 0);
+        scene.add(textoObjectoPerdido4CheckMesh);
+        textoObjectoPerdido4CheckMesh.visible = false
+
+        /* Adicionar o objeto 5 */
+        let textoObjectoPerdido5Mesh = new THREE.Mesh(textGeometryObjecto5, textMaterial);
+        textoObjectoPerdido5Mesh.position.set(5, 4.5, 0);
+        scene.add(textoObjectoPerdido5Mesh);
+
+        /* Marcar como found */
+        textoObjectoPerdido5CheckMesh = new THREE.Mesh(textGeometryObjecto5Check, textMaterial2);
+        textoObjectoPerdido5CheckMesh.position.set(4, 4.5, 0);
+        scene.add(textoObjectoPerdido5CheckMesh);
+        textoObjectoPerdido5CheckMesh.visible = false
+
+    });
+
+    /* Carregar a Cereal */
+    loader.load("./GLTFs/sceneLevelTwo/cereal/scene.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        cerealScene = gltf.scene
+        cereal = gltf.scene.children[0];
+        cereal.position.set(11.310, 2.070, -10);
+        cereal.rotation.set(-4.659, -Math.PI, -Math.PI);
+        cereal.scale.set(0.15, 0.1, 0.3)
+        gltf.scene.position.set(-5.390, 0, 0);
+
+        domEvents.addEventListener(cereal, 'click', event => {
+            console.log("Cereal foi encontrada!");
+            scene.remove(cerealScene)
+            textoObjectoPerdido1CheckMesh.visible = true
+        });
+
+    });
+    loader.load("./GLTFs/sceneLevelTwo/cafetera/scene.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        cafeteraScene = gltf.scene
+        cafetera = gltf.scene.children[0];
+        cafetera.position.set(6.820, 2.590, -13.270);
+        cafetera.rotation.set(-4.659, -Math.PI, 0.000);
+        cafetera.scale.set(0.030, 0.030, 0.030)
+        gltf.scene.position.set(-5.390, 0, 0);
+
+        domEvents.addEventListener(cafetera, 'click', event => {
+            console.log("Cafetera foi encontrada!");
+            scene.remove(cafeteraScene)
+            textoObjectoPerdido2CheckMesh.visible = true
+        });
+
+    });
+    loader.load("./GLTFs/sceneLevelTwo/bolachas/scene.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        bolachaScene = gltf.scene
+        bolacha = gltf.scene.children[0];
+        bolacha.position.set(9.880, 2.170, -15.070);
+        bolacha.rotation.set(-4.659, -Math.PI, 0.000);
+        bolacha.scale.set(0.030, 0.030, 0.030)
+        gltf.scene.position.set(-5.390, 0, 0);
+
+        domEvents.addEventListener(bolacha, 'click', event => {
+            console.log("Bolacha foi encontrada!");
+            scene.remove(bolachaScene)
+            textoObjectoPerdido3CheckMesh.visible = true
+        });
+
+    });
+    loader.load("./GLTFs/sceneLevelTwo/ovo_partido/scene.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        ovoScene = gltf.scene
+        ovo = gltf.scene.children[0];
+        ovo.position.set(4.100, 2.250, -9.920);
+        ovo.rotation.set(-4.659, -Math.PI, 0.000);
+        ovo.scale.set(0.001, 0.001, 0.001)
+        gltf.scene.position.set(-5.390, 0, 0);
+
+        domEvents.addEventListener(ovo, 'click', event => {
+            console.log("Ovo Partido foi encontrada!");
+            scene.remove(ovoScene)
+            textoObjectoPerdido4CheckMesh.visible = true
+        });
+
+    });
+
+    loader.load("./GLTFs/sceneLevelTwo/papel/scene.gltf", function(gltf) {
+        scene.add(gltf.scene);
+        papelScene = gltf.scene
+        papel = gltf.scene.children[0];
+        papel.position.set(17.510, 2.110, -9.370);
+        papel.rotation.set(-4.659, -Math.PI, 0.000);
+        papel.scale.set(0.010, 0.010, 0.030)
+        gltf.scene.position.set(-5.390, 0, 0);
+
+        domEvents.addEventListener(papel, 'click', event => {
+            console.log("Papel foi encontrada!");
+            scene.remove(papelScene)
+            textoObjectoPerdido5CheckMesh.visible = true
+        });
+
+    });
+
+
+    animate();
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
